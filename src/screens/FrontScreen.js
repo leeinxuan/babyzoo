8d5c50b28929124d57ba23b20a1933a8c509b410
inxuan <x14725836961@gmail.com>
2024-04-15 19:36:20 +0800

    logo

--- src/screens/FrontScreen.js
+++ src/screens/FrontScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { ScrollView, Text } from "react-native";
-import { Center } from "@gluestack-ui/themed";
+import { ScrollView, Text, Image } from "react-native";
+import { Center,Box } from "@gluestack-ui/themed";
 import ActivityList from "../component/ActivityList";
 import NewsList from "../component/NewsList";
 import Weather from "../component/Weather";
@@ -13,10 +13,15 @@ const FrontScreen = ({ navigation }) => {
     <ScrollView
       showsVerticalScrollIndicator={false}
       style={{ flex: 1, backgroundColor: '#FFF2C5' }}>
-      <Center>
-        <Text style={{ fontSize: 36, fontWeight: '900', color: '#B1D9DE', textAlign: 'center' }}>
+      <Center >
+        <Image
+          source={{url:''}}
+          style={{ width:190,height:100 }}
+
+        />
+        {/* <Text style={{ fontSize: 36, fontWeight: '900', color: '#B1D9DE', textAlign: 'center' }}>
           BABY{'\n'}ZOO
-        </Text>
+        </Text> */}
       </Center>
       <Weather />
       <Searchbar />
